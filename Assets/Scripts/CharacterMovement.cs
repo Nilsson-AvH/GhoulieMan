2d94c7a8196616392b438fe5fa321a526e67589c
NilssonLoRi <nlopez153@misena.edu.co>
2023-10-06 17:06:10 -0500

    Add changes to page 133

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -18,12 +18,19 @@ public class CharacterMovement : MonoBehaviour
     public float groundRadius = 0.2f;
     public LayerMask whatIsGround;
 
+    public float knifeSpeed = 600.0f;
+    public Transform knifeSpawn;
+    public Rigidbody knifePrefab;
+    
+    Rigidbody clone;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody> ();
         anim = GetComponent<Animator>();
         groundCheck = GameObject.Find("GroundCheck").transform;
+        knifeSpawn = GameObject.Find("KnifeSpawn").transform;
     }
 
     // Update is called once per frame
@@ -49,6 +56,12 @@ public class CharacterMovement : MonoBehaviour
             Flip();
         }
         anim.SetFloat ("Speed", Mathf.Abs (moveDirection));
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Attack();
+        }
+        
     }
 
     void Flip()
@@ -56,4 +69,11 @@ public class CharacterMovement : MonoBehaviour
         facingRight = !facingRight;
         transform.Rotate(Vector3.up, 180.0f, Space.World);
     }
+    
+    void Attack()
+    {
+        clone = Instantiate (knifePrefab, knifeSpawn.position, knifeSpawn.rotation) as Rigidbody;
+        clone.AddForce (knifeSpawn.transform.right * knifeSpeed);
+    }
+    
 }
